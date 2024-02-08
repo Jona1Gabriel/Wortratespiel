4809cee2325d96fc3261d5302133d558198be0b6
JonaGabriel <jgwoeh88@gmail.com>
2024-02-08 09:09:05 +0100

    anlegen der Wortliste

---
+++ words.js
@@ -0,0 +1,46 @@
+const wordList = [
+    {
+        word: "apple",
+        Hinweis: "Führender Handyhersteller"
+    },
+    {
+        word: "toyota",
+        Hinweis: "Japanischer Automobilkonzern"
+    },
+    {
+        word: "berlin",
+        Hinweis: "Hauptstadt Deutschlands"
+    },
+    {
+        word:"google",
+        Hinweis: "Suchmaschine und Internet-Unternehmen"
+    },
+    {
+        word:"samsung",
+        Hinweis: "Südkoreanischer Elektronikkonzern"
+    },
+    {
+        word:"facebook",
+        Hinweis: "Social Media-Plattform"
+    },
+    {
+        word:"russland",
+        Hinweis: "Land mit der größten Bevölkerung in Europa"
+    },
+    {
+        word:"bayern",
+        Hinweis:"Größtes Bundesland in Deutschland"
+    },
+    {
+        word:"wien",
+        Hinweis:"Hauptstadt Österreichs"
+    },
+    {
+        word:"grammy",
+        Hinweis:"US-amerikanischer Musikpreis"
+    },
+    {
+        word:"nasa",
+        Hinweis:"USA-amerikanische Weltraumagentur"
+    },
+]
